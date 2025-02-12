749c84c7851b291045f3a9638385824879652969
Marwa Ashoor <marwaashoor@homtail.com>
2025-02-12 12:45:57 +0300

    simpler

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 from datetime import datetime
 
 app = Flask(__name__)
@@ -12,16 +12,16 @@ def fetch_time():
     current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     return current_time
 
-@app.route('/upload_image', methods=['POST'])
-def upload_image():
-    if 'file' not in request.files:
-        return 'No file part'
-    file = request.files['file']
-    if file.filename == '':
-        return 'No selected file'
-    if file:
-        file.save(f"static/{file.filename}")
-        return f"Image uploaded: <img src='/static/{file.filename}' alt='Uploaded Image'>"
+# @app.route('/upload_image', methods=['POST'])
+# def upload_image():
+#     if 'file' not in request.files:
+#         return 'No file part'
+#     file = request.files['file']
+#     if file.filename == '':
+#         return 'No selected file'
+#     if file:
+#         file.save(f"static/{file.filename}")
+#         return f"Image uploaded: <img src='/static/{file.filename}' alt='Uploaded Image'>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
