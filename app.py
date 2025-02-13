8914e3fb7819dab32c6b9149fd980fe83b1acf2f
Marwa Ashoor <marwaashoor@homtail.com>
2025-02-13 10:33:25 +0300

    adjust

--- app.py
+++ app.py
@@ -8,9 +8,9 @@ def home():
     return render_template("index.html")
 
 @app.route("/get-age", methods=["GET"])
-def get_time():
+def get_age():
     current_age = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    return jsonify({"time": current_time})
+    return jsonify({"age": current_age})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
